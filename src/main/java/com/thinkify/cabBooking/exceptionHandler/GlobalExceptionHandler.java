edeb78c827e2c6144463c5f476b3411a9fe7b776
0Raj <rajasekark0@gmail.com>
2022-10-17 19:55:03 +0530

    Global Exception Handler
    update global exception handler

--- src/main/java/com/thinkify/cabBooking/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/thinkify/cabBooking/exceptionHandler/GlobalExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.thinkify.cabBooking.exceptionHandler;
 
+import com.thinkify.cabBooking.exception.*;
 import com.thinkify.cabBooking.module.ExceptionMsg;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.firewall.RequestRejectedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
@@ -17,10 +19,54 @@ import java.time.LocalDateTime;
 public class GlobalExceptionHandler {
 
 
-    @ExceptionHandler(NoHandlerFoundException.class)
-    public ResponseEntity<ExceptionMsg> noHandler(NoHandlerFoundException noHandler, WebRequest request) {
+    @ExceptionHandler(UserAlreadyPresentException.class)
+    public ResponseEntity<ExceptionMsg> userAlreadyPresentException(UserAlreadyPresentException exception, WebRequest request) {
         ExceptionMsg errorDetails = new ExceptionMsg(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(),
-                "Please enter valid URL", request.getDescription(false));
+                exception.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ExceptionMsg> userNotFoundException(UserNotFoundException exception, WebRequest request) {
+        ExceptionMsg errorDetails = new ExceptionMsg(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(),
+                exception.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(NoStrongPasswordException.class)
+    public ResponseEntity<ExceptionMsg> noStrongPassword(NoStrongPasswordException exception, WebRequest request) {
+        ExceptionMsg errorDetails = new ExceptionMsg(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(),
+                exception.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(DriverNotFoundException.class)
+    public ResponseEntity<ExceptionMsg> driverNotFound(DriverNotFoundException exception, WebRequest request) {
+        ExceptionMsg errorDetails = new ExceptionMsg(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(),
+                exception.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(DriverAlreadyPresentException.class)
+    public ResponseEntity<ExceptionMsg> driverAlreadyPresent(DriverAlreadyPresentException exception, WebRequest request) {
+        ExceptionMsg errorDetails = new ExceptionMsg(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(),
+                exception.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(NoCabFoundException.class)
+    public ResponseEntity<ExceptionMsg> noCabFound(NoCabFoundException exception, WebRequest request) {
+        ExceptionMsg errorDetails = new ExceptionMsg(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(),
+                exception.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<ExceptionMsg> noHandler(NoHandlerFoundException exception, WebRequest request) {
+        ExceptionMsg errorDetails = new ExceptionMsg(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(),
+                exception.getMessage(), request.getDescription(false));
 
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
@@ -46,11 +92,11 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ExceptionMsg> myMANVExceptionHandler(
-            MethodArgumentNotValidException methodArgumentNotValidException) {
+    public ResponseEntity<ExceptionMsg> myMANVExceptionHandler(MethodArgumentNotValidException exception,WebRequest request) {
 
-        ExceptionMsg errorDetails = new ExceptionMsg(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), "Validation Error",
-                methodArgumentNotValidException.getMessage());
+        ExceptionMsg errorDetails = new ExceptionMsg(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(),exception.getMessage()
+                ,request.getDescription(false)
+                );
 
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
 
@@ -61,7 +107,7 @@ public class GlobalExceptionHandler {
 
         ExceptionMsg exceptionMsg = new ExceptionMsg(LocalDateTime.now(),
                 HttpStatus.METHOD_NOT_ALLOWED.value(),
-                "BAD REQUEST",
+                exception.getMessage(),
                 request.getDescription(false));
 
         return new ResponseEntity<>(exceptionMsg, HttpStatus.METHOD_NOT_ALLOWED);
